f326dd08875a5c7e7bcefedf246494d3fd64fe4b
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2023-03-23 21:19:23 -0300

    ex testes

--- trabalho-Calculadora-modelo-main/org-teste-trabalhos.calculadora/src/test/java/calculadora/CalculadoraTest.java
+++ trabalho-Calculadora-modelo-main/org-teste-trabalhos.calculadora/src/test/java/calculadora/CalculadoraTest.java
@@ -1,16 +1,23 @@
 package calculadora;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
 	private Calculadora calc;
-	
-	
+
+	@BeforeEach
+	public void before(){
+		calc = new Calculadora();
+	}
+
 	@Test
 	public void testarConstrutorSemParametroIniciaMemoriaComZero() {
-		calc = new Calculadora();
+		 calc = new Calculadora();
 		//definir cenário
 		int memoriaEsperada = 0;
 
@@ -18,6 +25,91 @@ public class CalculadoraTest {
 		int memoriaObtida = calc.getMemoria();
 		
 		//comparar
-		assertEquals(resultadoEsperado, resultadoObtido);
+
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+	@Test
+	public void testarConstrutorComParametro0(){
+		calc = new Calculadora(0);
+		int memoriaEsperada = 0;
+
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+	@Test
+	public void testarConstrutorComParametroNegativo(){
+		calc = new Calculadora(-10);
+		int memoriaEsperada = -10;
+
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+	@Test
+	public void testarConstrutorComParametroPositivo(){
+		calc = new Calculadora(10);
+		int memoriaEsperada = 10;
+
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+	@Test
+	public void testarMetodoSomarNumeroNegatvo(){
+		calc.somar(-5);
+		int memoriaEsperada = -5;
+
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+
+	@Test
+	public void testarMetodoSubtrairPositivo(){
+		calc.subtrair(5);
+		int memoriaEsperada = 5;
+
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+
+	@Test
+	public void testarMetodoMultiplicarPositivo(){
+		calc.subtrair(3);
+		int memoriaEsperada = 0;
+
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada, memoriaObtida);
+	}
+
+	@Test
+	public void testarMetodoMDividirPor0() {
+		assertThrows(Exception.class, () -> calc.dividir(0));
+	}
+
+	@Test
+	public void testarMetodoMDividirPorPositivo() throws Exception {
+		calc = new Calculadora(9);
+		calc.dividir(3);
+		int memoriaEsperada = 3;
+		int memoriaObtida = calc.getMemoria();
+		assertEquals(memoriaEsperada,memoriaObtida);
+	}
+
+	@Test
+	public void testarMetodoExponenciar1(){
+		Calculadora calc = new Calculadora(1);
+
+		assertThrows(Exception.class, ()->calc.exponenciar(1));
+	}
+
+	@Test
+	public void testarMetodoExponenciarPor10() {
+		Calculadora calc = new Calculadora(1);
+
+		assertThrows(Exception.class, ()->calc.exponenciar(11));
+	}
+
+	@Test
+	public void testarMetodoZerarMemoria(){
+		Calculadora calc = new Calculadora(1);
+
 	}
 }

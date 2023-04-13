4817f0f9aee2f515c783fa46411fbdf9984bd6b5
Carlos Cayke B Nakazone <carlos.nakazone@estudante.iftm.edu.br>
2023-04-13 17:08:42 -0300

    Testes de funcionarios tercerizados prontos

--- Atividade3/src/test/java/pessoa/test/FuncionarioTercerizadoTest.java
+++ Atividade3/src/test/java/pessoa/test/FuncionarioTercerizadoTest.java
@@ -1,4 +1,46 @@
 package pessoa.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
+import pessoa.entity.Funcionario;
+import pessoa.entity.FuncionarioTercerizado;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FuncionarioTercerizadoTest {
+
+    private FuncionarioTercerizado funcionarioTercerizado;
+    @BeforeEach
+    public void funcTercerizado(){
+        funcionarioTercerizado = new FuncionarioTercerizado(200);
+    }
+    @Test
+    public void testarConstrutorEntradaDespesasInvalida() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            FuncionarioTercerizado funcionario = new FuncionarioTercerizado(-1000);
+        });
+    }
+    @Test
+    public void testarConstrutorEntradasValida(){
+         FuncionarioTercerizado funTercerizado = new FuncionarioTercerizado(500);
+        assertEquals(500,funTercerizado.getDespesasAdicionais());
+    }
+
+    @Test
+    public void testarModificarDespesasEntradaInvalida(){
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            FuncionarioTercerizado funcionarioTercerizado = new FuncionarioTercerizado(1200);
+        });
+    }
+
+    @Test
+    public void testarModificarDespesasEntradaValida(){
+        FuncionarioTercerizado funTercerizado = new FuncionarioTercerizado(500);
+        assertEquals(500,funTercerizado.getDespesasAdicionais());
+    }
+
+
 }
+

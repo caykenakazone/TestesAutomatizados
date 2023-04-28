671f9663fe67a8d35312936c71f20455594db4b5
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2023-04-27 21:24:26 -0300

    começando ex

---
+++ cliente-teste-modelo-main/projeto_cliente_teste/src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.iftm.client.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iftm.client.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
